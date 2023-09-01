f206f85e0664ee3b73b2839346c07465bf50d7a5
Kory Fong <kory.fong@private-ai.com>
2023-09-01 15:09:07 -0400

    Initial commit public helm chart

---
+++ helper_scripts/create-kubesecret.sh
@@ -0,0 +1,4 @@
+kubectl create secret docker-registry crprivateaiprod-creds \
+    --docker-server=crprivateaiprod.azurecr.io \
+    --docker-username=cf1ba98e-6727-4789-a986-3503a097fb84 \
+    --docker-password=DdH8Q~iUjTIB1oyr8kkB--8z79xqgn5BAWHasdgS
