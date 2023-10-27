07c555f0b53f673586afd4401b647ddc44151333
koryf <101284003+koryf@users.noreply.github.com>
2023-10-27 16:47:06 -0400

    Update create-kubesecret.sh with comment on placeholder credentials

--- helper_scripts/create-kubesecret.sh
+++ helper_scripts/create-kubesecret.sh
@@ -1,3 +1,5 @@
+# This is a helper script to create a secret for your helm chart. Please replace your username and password with valid credentials
+
 kubectl create secret docker-registry crprivateaiprod-creds \
     --docker-server=crprivateaiprod.azurecr.io \
     --docker-username=cf1ba98e-6727-4789-a986-3503a097fb84 \
